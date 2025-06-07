41441590e15ee39f28b8e4f288c630e10b6fbf14
Bishes Adhikari <specialbishes9@gmail.com>
2025-06-07 21:03:21 +1000

    data fetched and stored in the redux state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,31 @@
 import { Row, Col } from "react-bootstrap";
 
 import ProductCard from "../components/ProductCard";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import { Loader } from "../components/ui/Loader";
+import { Message } from "../components/ui/Message";
 
 const HomePage = () => {
-  // state to store the products
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  // useEffect to fetch data
-  useEffect(() => {
-    const fetchedProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchedProducts();
-  }, []);
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products?.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <ProductCard product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error?.data.message || error.error}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products?.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <ProductCard product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,75 +3,80 @@ import { Link, useParams } from "react-router-dom";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
 import Rating from "../components/Rating";
 import { IoMdArrowRoundBack } from "react-icons/io";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useGetProductsDetailsQuery } from "../slices/productsApiSlice";
+import { Loader } from "../components/ui/Loader";
+import { Message } from "../components/ui/Message";
 
 const ProductPage = () => {
-  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
 
-  useEffect(() => {
-    const fetchedProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
-    fetchedProduct();
-  }, [productId]);
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductsDetailsQuery(productId);
 
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
         <IoMdArrowRoundBack /> Go Back
       </Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item> Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>Description:{product.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error?.data?.message || error.message}</Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status</Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                    </strong>
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroup.Item>
-              <ListGroup.Item className="d-grid">
-                <Button type="button" disabled={product.countInStock === 0}>
-                  Add to Cart
-                </Button>
-              </ListGroup.Item>
+              <ListGroup.Item> Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>Description:{product.description}</ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-grid">
+                  <Button type="button" disabled={product.countInStock === 0}>
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
