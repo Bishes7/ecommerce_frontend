7fdc188267b1ae82d8ef1a141b24675749dd9406
Bishes Adhikari <specialbishes9@gmail.com>
2025-06-10 16:09:27 +1000

    car slice feature completed  in redux

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -2,7 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItem: [] };
+  : { cartItems: [] };
+
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -11,11 +15,39 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
 
-      const existItem = state.cartItem.find(
+      const existItem = state.cartItems.find(
         (product) => product._id === item._id
       );
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((product) =>
+          product._id === existItem._id ? item : product
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      // Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      // calculate shipping price - over 100 $- free else 10$ charge
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      // calculate tax price
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+
+      // calcuate total price
+      Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice);
+
+      // save in local storage
+      localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
